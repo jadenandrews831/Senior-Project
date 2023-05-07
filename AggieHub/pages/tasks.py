2c2085a000813ce7fabcda0aeae9e581a56eed6e
DougGajda <112100715+DougGajda@users.noreply.github.com>
2023-05-07 10:24:30 -0400

    Doxygen Comments for the Views.py and Tasks.py

--- AggieHub/pages/tasks.py
+++ AggieHub/pages/tasks.py
@@ -7,43 +7,56 @@
 # print(f'{parent}/AggieHub/pages/')
 # sys.path.insert(0, f'{parent}/pages/')
 
-<<<<<<< HEAD
-#from scrAApe import *
-=======
 # from scrAApe import *
->>>>>>> e1e26fa04e2c449b96c4bce0ba06e4d53ac9bbb1
 from .scrAApe import *
 
 success = None
 
-#verify user credentials
+
+##
+#task1 function
+#
+#Takes in banner ID and pin from login form and verifies user credentials.
 def task1(banner, pin):
     global success
     success = Authenticate(banner, pin)
     return success.auth
 
-#get terms
+
+##
+#task2 function
+#
+#Gets the desired term by pulling it off of Aggie Access.
 def task2():
     global success
     scrape = ScrAApe(success)
     terms = scrape.get_terms()
     return terms
 
-#get subjects
+##
+#task3 function
+#
+#Gets the subjects off of Aggie Access with the term that was selected prior.
 def task3(term):
     global success
     scrape = ScrAApe(success)
     subjects = scrape.get_subject(term)
     return list(subjects.keys())
 
-#get courses
+##
+#task4 function
+#
+#Gets the courses off of Aggie Access with the subject that was selected prior.
 def task4(subject):
     global success
     scrape = ScrAApe(success)
     courses = scrape.get_course(subject)
     return courses
 
-#pulls all sections for selected course
+##
+#task5 function
+#
+#Gets all the sections for the selected course from Aggie Access.
 def task5(course):
     global success
     scrape = ScrAApe(success)
@@ -51,7 +64,10 @@ def task5(course):
     print(sections)
     return sections
 
-#pulls student information
+##
+#task6 function
+#
+#Gets the student information off of Aggie Access.
 def task6():
     global success
     scrape = ScrAApe(success)
@@ -59,6 +75,10 @@ def task6():
     #print(profile)
     return profile
 
+##
+#task7 function
+#
+#Register's the student in Aggie Access.
 def task7(schedule):
     global success
     scrape = ScrAApe(success)
--- AggieHub/pages/views.py
+++ AggieHub/pages/views.py
@@ -10,12 +10,22 @@ from .forms import *
 from .tasks import *
 from .authentication import StudentBackend
 
+##
+#home function 
+#
+#Checks to make sure that the request has the correct banner id in order to allow access to the home page.
 def home(request):
     if(request.session.has_key('banner_id')):
         return render(request, 'home.html')
     else:
         return redirect('login')
 
+##
+#login function
+#
+#Takes in the form input of the student's banner ID and their pin and authenticates it with Aggie Access.
+#Should both inputs be valid, then it will take them to the home function. If input isnt valid, it will post a messege
+#to user that "Banner ID or PIN is invalid. Please try again.".
 def login(request):
     if (request.method == 'POST'): 
         form = loginForm(request.POST)
@@ -29,7 +39,11 @@ def login(request):
                 return redirect('login')
             
     return render(request, 'login.html', {'form': loginForm})
-    
+
+##
+#get_subjects function
+#
+#Checks if the term has been posted, then lists out the subjects for that term.
 def get_subjects(request):
     if (request.method == 'POST'):
         term = request.POST['term']
@@ -38,6 +52,10 @@ def get_subjects(request):
         }
     return JsonResponse(response)
 
+##
+#get_courses function
+#
+#Checks if the subject has been posted, then lists out the courses for the selected subject.
 def get_courses(request):
     if (request.method == 'POST'):
         subject = request.POST['subject']
@@ -48,6 +66,10 @@ def get_courses(request):
         }
     return JsonResponse(response)
 
+##
+#get_sections function
+#
+#Checks if the course has been posted, then lists out the sections for that course.
 def get_sections(request):
     if (request.method == 'POST'):
         course = request.POST['course']
@@ -56,10 +78,18 @@ def get_sections(request):
         }
     return JsonResponse(response)
 
+##
+#get_profile function
+#
+#Outputs the profile data of the student.
 def get_profile(request):
     if (request.method == 'POST'):
         return JsonResponse(task6().__dict__)
 
+##
+#get_terms function
+#
+#Lists out the available terms to be selected by the student.
 def get_terms(request):
     if (request.method == 'POST'):
         terms = task2()
@@ -68,25 +98,43 @@ def get_terms(request):
             'p_term': list(terms.values())
         }
         return JsonResponse(response)
-    
+
+##
+#register_student function
+#
+#Takes the created schedule made by the student and submits the CRN to Aggie Access to register student.
 def register_student(request):
     if (request.method == 'POST'):
         schedule = request.POST['schedule']
         response = task7((json.loads(schedule)['pkg']))
         return JsonResponse(response)
 
+##
+#override
+#
+#Checks to make sure that the request has the correct banner id in order to allow access to the override page.
 def override(request):
     if(request.session.has_key('banner_id')):
         return render(request, 'override.html')
     else:
         return redirect('login')
 
+##
+#override
+#
+#Checks to make sure that the request has the correct banner id in order to allow access to the guides page.
 def guides(request):
     if(request.session.has_key('banner_id')):
         return render(request, 'guides.html')
     else:
         return redirect('login')
 
+##
+#override
+#
+#Checks to make sure that the request has the correct banner id in order to allow access to the contact page.
+#This function also controls the IT helpdesk form submission should the student need to reach out for IT support for the website.
+#It takes in the fields: email, topic, and message. After being submitted, it posts a message to the student "Form submission successful".
 def contact(request):
     if(request.session.has_key('banner_id')):
         if (request.method == 'POST'):
@@ -102,6 +150,10 @@ def contact(request):
     else:
         return redirect('login')
 
+##
+#logout function
+#
+#Logs out the student from the authenticated session and takes them back to the login page.
 def logout(request):
     del request.session['banner_id']
     return redirect('login')
